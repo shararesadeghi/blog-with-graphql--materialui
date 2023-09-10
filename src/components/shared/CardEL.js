5c7d29b15d79dca02af96ab81e5184cbba26c83b
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-09-10 18:27:51 +0430

    cards for blogs created

--- src/components/shared/CardEL.js
+++ src/components/shared/CardEL.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography, Button } from '@mui/material';
 
-const CardEL = (props) => {
+const CardEL = ({title, slug, coverPhoto, author}) => {
     return (
-        <div>
-            cardel
-        </div>
+        <Card sx={{boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px" , borderRadius: 4}}>
+            <CardHeader
+                avatar={<Avatar src={author.avatar.url} sx={{marginLeft:2}}/>}
+                title={<Typography component="p" variant="p" color="text.secondary">
+                           {author.name}
+                        </Typography>}
+            />
+            <CardMedia component="img" height="194" image={coverPhoto.url} alt={slug}/>
+            <CardContent>
+                <Typography component="h3" variant="h6" color="text.primary" fontWeight={600}>{title}</Typography>
+            </CardContent>
+            <Divider variant="middle" sx={{margin: "10px"}}/>
+            <CardActions>
+                <Button variant="outlined" size="small" sx={{width: "100%", borderRadius: 3}}>مطالعه مقاله</Button>
+            </CardActions>
+        </Card>
     );
 };
 
