a69b67c7899c4fecf79efb9a11c3f7b2622a1158
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-09-11 17:19:18 +0430

    authors componenttand query created

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_AUTHORS_INFO } from '../../graphql/queries';
+import { Grid, Avatar, Typography, Divider } from '@mui/material';
 
 const Authors = () => {
+
+    const {loading,data, errors} = useQuery(GET_AUTHORS_INFO);
+
+    if(loading) return <h4>Loading...</h4>;
+    if(errors) return <h4>Errors ...</h4>;
+
     return (
-        <div>
-            authors
-        </div>
+        <Grid container sx={{boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px" , borderRadius: 4}}>
+            {
+                data.authors.map((author,index)=>( 
+                    <React.Fragment key={author.id}>
+                <Grid item xs={12} padding={2} >
+                    <a href={`/authors/${author.slug}`} style={{display:"flex", alignItems:"center", textDecoration:"none"}}>
+                    <Avatar src={author.avatar.url} sx={{marginLeft:2}}/>
+                    <Typography component="p" variant="p" color="text.secondary">
+                        {author.name}
+                    </Typography>
+                    </a>
+                </Grid>
+                {
+                    index !== data.authors.length - 1 && (
+                <Grid item xs={12}>
+                    <Divider variant="middle" />
+                </Grid>
+                    )
+                }
+                </React.Fragment>
+                ))
+            }
+
+        </Grid>
     );
 };
 
