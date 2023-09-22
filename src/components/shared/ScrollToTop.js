3428083b228cd8fdf42fc362ad11f919e0454533
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-09-22 17:33:11 +0330

    project completed

--- src/components/shared/ScrollToTop.js
+++ src/components/shared/ScrollToTop.js
@@ -1,8 +1,8 @@
 import {useEffect} from 'react';
-import {useLoaction} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
 
 const ScrollToTop = ()=>{
-    const {pathname} = useLoaction();
+    const {pathname} = useLocation();
 
     useEffect(()=>{
         window.scrollTo(0,0);
